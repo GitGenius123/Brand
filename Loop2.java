3f5b4a282eabbe697a7b84656059c2243b5e676d
M Amin <156469055+GitGenius123@users.noreply.github.com>
2024-01-19 03:37:45 +0500

    Add files via upload

---
+++ Loop2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Loop2{
+	public static void main(String args[]){
+		int i=1;
+		
+		while(i<6){
+			System.out.println(i+". Hello World");
+			i++;
+		}
+		
+		i=10;
+		
+		while(i>0){
+			System.out.print(i+" ");
+			i--;
+		}
+		
+		System.out.println();
+		
+		i=2;
+		while(i<22){
+			System.out.print(i +" ");
+			i+=2;
+		}
+		
+	}
+}
